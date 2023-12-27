ba655698aa86ad40ece9557bfaf2f5c50b61817f
Beerimnd01 <beerimnd01@gmail.com>
2023-12-27 15:20:01 +0530

    changes

--- src/basics06/Lab081.java
+++ src/basics06/Lab081.java
@@ -36,7 +36,7 @@ public class Lab081 {
 
         }
 
-        
+
             }
 
 
--- src/basics06/Lab082.java
+++ src/basics06/Lab082.java
@@ -1,2 +1,20 @@
-package src.basics06;public class Lab082 {
+package src.basics06;
+
+import java.util.Scanner;
+
+public class Lab082 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int num=sc.nextInt();
+        long fact = 1;
+
+
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
+
+
+        }
+        System.out.println(fact);
+    }
 }
